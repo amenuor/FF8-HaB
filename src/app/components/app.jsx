ec3a003367d4d1c85a5f15835a12c2e978b5e647
Angelo Agatino Nicolosi <angelonicolosi@yanchware.com>
2016-08-30 01:44:33 +0200

    Additional cleanup

--- src/app/components/app.jsx
+++ src/app/components/app.jsx
@@ -1,9 +1,4 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router';
-import { connect } from 'react-redux';
-import { bindActionCreators  } from 'redux';
-import { fetchEntities }  from '../actions/entity_actions';
-import { currentUser } from '../utils/localstorage';
 
 class App extends Component {
 
